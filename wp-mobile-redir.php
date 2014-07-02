e8f53a6754312624c6bfb7f4b457ade41c65bfc8
Copypanthers website <copypanthers@ip-172-31-41-222.eu-west-1.compute.internal>
2014-07-02 17:31:31 +0000

    Redirect the mobile user to mobile page.

---
+++ wp-mobile-redir.php
@@ -0,0 +1,45 @@
+<?php
+
+
+//ini_set("display_errors",true);
+/*
+Plugin Name: CP Mobile Redirect
+Description: Select a URL to point mobile users to
+Author: Mehmet YILIDZ
+Version: 1.0
+Author URI: http://copypanthers.com
+*/
+
+
+require_once __DIR__.'/../../../vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php';
+
+
+
+
+add_action("init","is_mobile");
+
+function is_mobile(){
+        $detect = new Mobile_Detect();
+        if ($detect->isMobile()){
+                if (substr(icl_get_home_url(), -1) !="/"){
+                    $current_url = icl_get_home_url()."/";
+                }
+                
+                $mobile_pages = [
+                    "en" => "mobile-index",
+                    "de" => "mobile-de",
+                    "fr" => "mobile-fr",
+                    "fi" => "mobile-fi",
+                    "tr" => "mobile-tr",
+                    "da" => "mobile-da",
+                    "nb" => "mobile-no",
+                    "sv" => "mobile-se"
+                ];
+                if (!preg_match("/mobile/",$_SERVER['REQUEST_URI'])){ 
+                	$tobe_redirected = $current_url.$mobile_pages[ICL_LANGUAGE_CODE];
+                	header("Location: $tobe_redirected");
+                	exit;
+		}
+        }
+}
+
